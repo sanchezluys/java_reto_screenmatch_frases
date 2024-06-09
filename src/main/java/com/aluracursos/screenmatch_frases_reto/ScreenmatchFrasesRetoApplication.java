f1b55d7646e1b75930106b3c99ce941fa75a2255
luys <sanchezluys@gmail.com>
2024-06-09 17:27:59 -0500

    iniciando el reto

---
+++ src/main/java/com/aluracursos/screenmatch_frases_reto/ScreenmatchFrasesRetoApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch_frases_reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchFrasesRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchFrasesRetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/screenmatch_frases_reto/ScreenmatchFrasesRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch_frases_reto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchFrasesRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
